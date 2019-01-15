e407ee95615250c06df78ae8b7fa0fcf34f6aee8
andre <andlucsil1221@gmail.com>
2019-01-15 21:07:56 -0200

    criada a aplicação no springboot

---
+++ probesserver/src/test/java/br/com/andlucsil/probesserver/main/test/MainTest.java
@@ -0,0 +1,27 @@
+package br.com.andlucsil.probesserver.main.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.andlucsil.probesserver.Main;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Main.class)
+public class MainTest {
+
+	@Test
+	public void inicializa(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
