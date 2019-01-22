ca7f0487370b4ebd08f634acd5096df3cdb6002d
andre <andlucsil1221@gmail.com>
2019-01-22 21:23:37 -0200

    adicionado leitura dos ultimos 5 valores e valor medio

--- probesserver/src/main/java/br/com/andlucsil/probesserver/controller/ProbesValuesController.java
+++ probesserver/src/main/java/br/com/andlucsil/probesserver/controller/ProbesValuesController.java
@@ -1,6 +1,10 @@
 package br.com.andlucsil.probesserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +23,7 @@ public class ProbesValuesController {
 	@Autowired
 	ProbesIdfRepository probesidfrepository;
 	
-	//Retorna todas as leituras
+	//Retorna todas as leituras de todos os sensores
 	@GetMapping("/")
 	public String lista(Model model){
 		//${reads} será a variável disponível no template thymeleaf
@@ -27,9 +31,25 @@ public class ProbesValuesController {
 		return "probesvalues/lista"; //arquivo .html dentro da pasta resources/templates
 	}
 	
+	//Retorna todas as leituras de um determinado sensor
 	@GetMapping("/{sensor}")
-	public String visualizar(@PathVariable String sensor, Model model){
+	public String visualizarSensor(@PathVariable String sensor, Model model){
 		model.addAttribute("reads", probesvaluesrepository.todasLeiturasSensor(sensor));
+		return "probesvalues/lista";
+	}
+	
+	//Retorna o valor médio da leitura de um sensor
+	@GetMapping("/{sensor}/avg")
+	public String visualizarMedia(@PathVariable String sensor, Model model){
+		model.addAttribute("avg", probesvaluesrepository.leituraMediaSensor(sensor));
+		return "probesvalues/avg";
+	}
+	
+	//Retorna as ultimas 5 leituras
+	@GetMapping("/{sensor}/lastfives")
+	public String visualizarUltimasCinco(@PathVariable String sensor, Model model){
+		Pageable cincoUltimos = PageRequest.of(0, 5);
+		model.addAttribute("reads", probesvaluesrepository.ultimasCincoLeituras(sensor, cincoUltimos));
 		return "probesvalues/lista";
 	}
 }
--- probesserver/src/main/java/br/com/andlucsil/probesserver/repository/ProbesValuesRepository.java
+++ probesserver/src/main/java/br/com/andlucsil/probesserver/repository/ProbesValuesRepository.java
@@ -2,8 +2,10 @@ package br.com.andlucsil.probesserver.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.andlucsil.probesserver.model.ProbesValues;
 
@@ -14,4 +16,18 @@ public interface ProbesValuesRepository extends JpaRepository <ProbesValues, Lon
 			"where pv.probesidf = pidf.id_value\n" + 
 			"and pidf.description = ?1")
 	List<ProbesValues> todasLeiturasSensor(String sensor);
+	
+	/*Recuperar a leitura media de um determinado sensor*/
+	@Query("select AVG(pv.read_value) from ProbesValues pv, ProbesIdf pidf\n" + 
+			"where pv.probesidf = pidf.id_value\n" + 
+			"and pidf.description = ?1")
+	Double leituraMediaSensor(String sensor);
+	
+	/*Recupera as ultimas 5 leituras de um ultimo sensor*/
+	@Query("select pv from ProbesValues pv, ProbesIdf pidf\n" + 
+			"where pv.probesidf = pidf.id_value\n" + 
+			"and pidf.description = :sensor "+
+			"order by pv.id desc")
+	List<ProbesValues> ultimasCincoLeituras(@Param("sensor")String sensor, Pageable pageable);
+	
 }
