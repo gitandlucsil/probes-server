eab209d99b4b7ec3afec77b363f6e139ae99f676
andre <andlucsil1221@gmail.com>
2019-01-22 22:00:14 -0200

    aparecendo o nome do sensor na tela

--- probesserver/src/main/java/br/com/andlucsil/probesserver/repository/ProbesIdfRepository.java
+++ probesserver/src/main/java/br/com/andlucsil/probesserver/repository/ProbesIdfRepository.java
@@ -3,10 +3,11 @@ package br.com.andlucsil.probesserver.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.andlucsil.probesserver.model.ProbesIdf;
 
 public interface ProbesIdfRepository extends JpaRepository <ProbesIdf, Long>{
 
-	List<ProbesIdf> findAll();
+	
 }
