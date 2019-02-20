65de0bc71a9d545301bab6125e545b77cb33be3f
andre <andlucsil1221@gmail.com>
2019-02-19 22:39:54 -0300

    apresentado registros individual dos alarmes no html

--- probesserver/src/main/java/br/com/andlucsil/probesserver/controller/AlarmRegisterController.java
+++ probesserver/src/main/java/br/com/andlucsil/probesserver/controller/AlarmRegisterController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.andlucsil.probesserver.repository.AlarmRegisterRepository;
@@ -18,8 +19,15 @@ public class AlarmRegisterController {
 	@GetMapping("/")
 	public String lista(Model model){
 		//${dados} será a variável disponível no template thymeleaf
-		model.addAttribute("dados",alarmRegisterRepository.findAll());
+		model.addAttribute("alarms",alarmRegisterRepository.findAll());
 		return "alarmregister/lista"; //arquivo .html dentro da pasta resources/templates
+	}
+	
+	//Retorna todos os alarmes de um determinado sensor
+	@GetMapping("/{sensor}")
+	public String visualizarSensor(@PathVariable String sensor, Model model){
+		model.addAttribute("alarms", alarmRegisterRepository.todosAlarmesSensor(sensor));
+		return "alarmregister/lista_individual";
 	}
 	
 }
--- probesserver/src/main/java/br/com/andlucsil/probesserver/repository/AlarmRegisterRepository.java
+++ probesserver/src/main/java/br/com/andlucsil/probesserver/repository/AlarmRegisterRepository.java
@@ -1,8 +1,18 @@
 package br.com.andlucsil.probesserver.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import br.com.andlucsil.probesserver.model.AlarmRegister;
 
 public interface AlarmRegisterRepository extends JpaRepository <AlarmRegister, Long>{
 
+	/*Recuperar todos os alarmes de um determinado sensor*/
+	@Query("select ar from AlarmRegister ar, ProbesValues pv, ProbesIdf pidf\n" + 
+			"where pv.probesidf = pidf.id_value\n" + 
+			"and ar.probesvalues = pv.id\n" + 
+			"and pidf.description = ?1")
+	List<AlarmRegister> todosAlarmesSensor(String sensor);
 }
